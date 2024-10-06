94474628d9012c7fe032346961b2885f5bac6bd8
hudakasim <030722088@std.izu.edu.tr>
2024-10-06 19:40:56 +0300

    staticMethods

---
+++ calc/src/Calc.java
@@ -0,0 +1,12 @@
+public class Calc {
+    // static mean that we can use the methods without create an object for iut
+    public static int add (int a, int b){
+        return a + b;
+    }
+    public static int sub (int a, int b){
+        return a -b;
+    }
+    public static int mul (int a, int b){
+        return a * b;
+    }
+}
---
+++ calc/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calc.add(12,4));
+        System.out.println(Calc.mul(12, 5));
+    }
+}
