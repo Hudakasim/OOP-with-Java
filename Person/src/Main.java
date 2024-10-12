424f05200b148a2e15a9edde4c95ff9fbc362a33
hudakasim <030722088@std.izu.edu.tr>
2024-10-12 20:41:48 +0300

    reference & obj type

--- Person/src/Main.java
+++ Person/src/Main.java
@@ -8,10 +8,22 @@ public class Main {
 //        Student S2 = new Student();
 
 
-        SalariedEmployee sel1 = new SalariedEmployee("Ahmet", 20, "Qatar", "Qatari", 25000,"prof" , "teacher", 5000,100 );
-        System.out.println(sel1.getSalary());
+//        SalariedEmployee sel1 = new SalariedEmployee("Ahmet", 20, "Qatar", "Qatari", 25000,"prof" , "teacher", 5000,100 );
+//        System.out.println(sel1.getSalary());
+//
+//        Employee E1 = new Employee("Ahmet", 20, "Qatar", "Qatari", 25000,"prof" , "teacher");
+//        E1.printAllInfo();
 
+        // reference type: Employee. obj type: Employee
         Employee E1 = new Employee("Ahmet", 20, "Qatar", "Qatari", 25000,"prof" , "teacher");
-        E1.printAllInfo();
+        // reference type: Employee. obj type: SalariedEmployee
+        Employee E2 = new SalariedEmployee("Huda", 22, "Palestine", "Palestinian", 10000,"intern" , "teacher", 1000,10 );
+
+        System.out.println(E1.getSalary());
+        System.out.println(E2.getSalary());
+
+        // System.out.println(E2.getBonus); ---> Error
+        // SalariedEmployee E3 = new Employee();  ---> Error, u can't change places between sub and super classes
+
     }
 }
