d4ed3de02d30da95d5a506b527fd519b797cbd96
hudakasim <030722088@std.izu.edu.tr>
2024-10-13 13:04:35 +0300

    protected variable

---
+++ Shape/src/Shape.java
@@ -0,0 +1,12 @@
+public class Shape {
+    protected double length;
+    protected double width;
+
+    public Shape(){
+
+    }
+    public Shape(double l, double w){
+        length = l;
+        width = w;
+    }
+}
